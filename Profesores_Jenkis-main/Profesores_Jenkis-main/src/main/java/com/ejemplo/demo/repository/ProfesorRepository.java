c3a7bd2b5f0c31645ad4e255606c1922e7e30918
DannaLopezB <dannalopezbravo10@gmail.com>
2025-05-30 06:53:40 -0500

    jenkins

--- Profesores_Jenkis-main/Profesores_Jenkis-main/src/main/java/com/ejemplo/demo/repository/ProfesorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ejemplo.demo.repository;
-
-import com.ejemplo.demo.model.Profesor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfesorRepository extends JpaRepository<Profesor, Long> {}
